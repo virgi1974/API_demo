25a78a0b05623dc3b94cda6976b67d6a68d68d24
virgilio1974 <virgifp@yahoo.es>
2016-02-03 16:24:02 +0100

    added the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
         # to route requests for different versioned representations.
         # We can do this effectively by using a route constraint that checks for a version specified 
         # in the request’s accept header.
-        resources :users, :only => [:show]
+        resources :users, :only => [:show, :create]
 
     end
 
