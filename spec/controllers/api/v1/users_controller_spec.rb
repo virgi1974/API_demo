1180335bc4eac5396ef522e59a247d5f8d06932c
virgilio1974 <virgifp@yahoo.es>
2016-02-03 18:02:04 +0100

    content-type of every request fixed to json

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -6,6 +6,7 @@ describe Api::V1::UsersController do
 
   # before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
   before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1, #{Mime::JSON}" }
+  before(:each) { request.headers['Content-Type'] = Mime::JSON.to_s }
 
    describe "GET #show" do
      before(:each) do
