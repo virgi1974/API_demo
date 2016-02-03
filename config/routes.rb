56b1ee1ae1181e58d6d95ce62f73c2d57050958e
virgilio1974 <virgifp@yahoo.es>
2016-02-03 12:30:59 +0100

    factoryGirl for user + first user tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,17 @@ Rails.application.routes.draw do
 
   devise_for :users
   # namespace for the API
+
+  # we need to tell Rails how
   namespace :api, defaults: { format: :json }, constraints: {subdomain: 'api'}, path: '/'  do 
 
+
+    # Since we don’t want to affect our URI structure for the resource,
+    #  we can use the :module scope to namespace the controller and not the URI.
       scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+        # to route requests for different versioned representations.
+        # We can do this effectively by using a route constraint that checks for a version specified 
+        # in the request’s accept header.
 
     end
 
--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -4,7 +4,13 @@ class ApiConstraints
     @default = options[:default]
   end
 
+  # We can use this constraint to route requests based on the version specified in the request’s accept header.
   def matches?(req)
     @default || req.headers['Accept'].include?("application/vnd.marketplace.v#{@version}")
+
+    # request
+    #       .headers
+    #       .fetch(:accept)
+    #       .include?("version=#{version}")
   end
 end
