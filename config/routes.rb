2c6fd51a52510bd897b657d16df9d538d7678949
virgilio1974 <virgifp@yahoo.es>
2016-02-03 11:42:50 +0100

    devise CORRECTLY added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
 
+  devise_for :users
   # namespace for the API
   namespace :api, defaults: { format: :json }, constraints: {subdomain: 'api'}, path: '/'  do 
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
-
+    
   end
+
 end
