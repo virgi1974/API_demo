ae53b775a2316b8ff88bb9220e7065a90c5613ac
virgilio1974 <virgifp@yahoo.es>
2016-02-03 17:55:55 +0100

    format json param refactored

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -4,12 +4,13 @@ require 'pry'
 # describe UsersController do --> we change it because we placed it in other folder
 describe Api::V1::UsersController do
 
-  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
+  # before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
+  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1, #{Mime::JSON}" }
 
    describe "GET #show" do
      before(:each) do
        @user = FactoryGirl.create :user
-       get :show, id: @user.id, format: :json
+       get :show, id: @user.id #, format: :json
      end
 
      it "returns the information about a reporter on a hash" do
@@ -25,7 +26,7 @@ describe Api::V1::UsersController do
        context "when is successfully created" do
          before(:each) do
            @user_attributes = FactoryGirl.attributes_for :user
-           post :create, { user: @user_attributes }, format: :json
+           post :create, { user: @user_attributes } #, format: :json
          end
 
          it "renders the json representation for the user record just created" do
@@ -41,7 +42,7 @@ describe Api::V1::UsersController do
            #notice I'm not including the email
            @invalid_user_attributes = { password: "12345678",
                                         password_confirmation: "12345678" }
-           post :create, { user: @invalid_user_attributes }, format: :json
+           post :create, { user: @invalid_user_attributes } #, format: :json
          end
 
          it "renders an errors json" do
@@ -64,7 +65,7 @@ describe Api::V1::UsersController do
            before(:each) do
              @user = FactoryGirl.create :user
              patch :update, { id: @user.id,
-                              user: { email: "newmail@example.com" } }, format: :json
+                              user: { email: "newmail@example.com" } } #, format: :json
            end
 
            it "renders the json representation for the updated user" do
@@ -79,7 +80,7 @@ describe Api::V1::UsersController do
            before(:each) do
              @user = FactoryGirl.create :user
              patch :update, { id: @user.id,
-                              user: { email: "bademail.com" } }, format: :json
+                              user: { email: "bademail.com" } } #, format: :json
            end
 
            it "renders an errors json" do
@@ -99,7 +100,7 @@ describe Api::V1::UsersController do
        describe "DELETE #destroy" do
          before(:each) do
            @user = FactoryGirl.create :user
-           delete :destroy, { id: @user.id }, format: :json
+           delete :destroy, { id: @user.id } #, format: :json
          end
 
          it { should respond_with 204 } #server successfully processed the request, but is not returning any content.
